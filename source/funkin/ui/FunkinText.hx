cc401bfb8f885c819d1ac7ac8c39880c234bf126
Yoshman29 <81191621+YoshiCrafter29@users.noreply.github.com>
2022-11-02 17:49:30 +0100

    update

--- source/funkin/ui/FunkinText.hx
+++ source/funkin/ui/FunkinText.hx
@@ -4,11 +4,13 @@ import flixel.util.FlxColor;
 import flixel.text.FlxText;
 
 class FunkinText extends FlxText {
-    public function new(X:Float = 0, Y:Float = 0, FieldWidth:Float = 0, ?Text:String, Size:Int = 16, EmbeddedFont:Bool = true) {
-        super(X, Y, FieldWidth, Text, Size, EmbeddedFont);
+    public function new(X:Float = 0, Y:Float = 0, FieldWidth:Float = 0, ?Text:String, Size:Int = 16, Border:Bool = true) {
+        super(X, Y, FieldWidth, Text, Size);
         setFormat(Paths.font("vcr.ttf"), Size, FlxColor.WHITE);
-        borderStyle = OUTLINE;
-        borderSize = 1;
-        borderColor = 0xFF000000;
+        if (Border) {
+            borderStyle = OUTLINE;
+            borderSize = 1;
+            borderColor = 0xFF000000;
+        }
     }
 }
