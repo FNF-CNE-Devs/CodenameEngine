500d3479a9e75d213a462e635bae8281ecf5071b
Yoshman29 <81191621+YoshiCrafter29@users.noreply.github.com>
2022-11-08 22:14:01 +0100

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,3 +1,4 @@
+haxelib git linc_discord-rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git flixel-yc29 https://www.github.com/YoshiCrafter29/flixel
 haxelib git hscript-improved https://www.github.com/YoshiCrafter29/hscript-improved
 haxelib install hxCodec
