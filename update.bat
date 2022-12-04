3e76a27e4093e4b6c65e983c2de7c6503f812067
Yoshman29 <81191621+YoshiCrafter29@users.noreply.github.com>
2022-12-04 19:10:16 +0100

    away3d

--- update.bat
+++ update.bat
@@ -1,4 +1,5 @@
 haxelib git linc_discord-rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git flixel-yc29 https://www.github.com/YoshiCrafter29/flixel
 haxelib git hscript-improved https://www.github.com/YoshiCrafter29/hscript-improved
+haxelib install away3d
 haxelib install hxCodec
