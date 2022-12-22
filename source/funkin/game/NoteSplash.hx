1ec1fea3f989fb9f56d1d53b48586916d76f608a
biomseed <73907153+biomseed@users.noreply.github.com>
2022-12-22 15:37:25 -0500

    Add note splash assets

--- source/funkin/game/NoteSplash.hx
+++ source/funkin/game/NoteSplash.hx
@@ -8,7 +8,7 @@ class NoteSplash extends FlxSprite
 	public function new(x:Float, y:Float, ?notedata:Int = 0)
 	{
 		super(x, y);
-		frames = Paths.getSparrowAtlas('noteSplashes');
+		frames = Paths.getSparrowAtlas('game/noteSplashes');
 		animation.addByPrefix('note1-0', 'note impact 1  blue', 24, false);
 		animation.addByPrefix('note2-0', 'note impact 1 green', 24, false);
 		animation.addByPrefix('note0-0', 'note impact 1 purple', 24, false);
