aec01488d547338950bd94cf5b8759bb4c5a6a46
biomseed <73907153+biomseed@users.noreply.github.com>
2022-12-22 16:57:16 -0500

    Removed this thing that doesnt work right
    Removed the functionality that adds a random number between -2 and 2 to the framerate of the animation, as it seems to bug out a little

--- source/funkin/game/NoteSplash.hx
+++ source/funkin/game/NoteSplash.hx
@@ -25,7 +25,6 @@ class NoteSplash extends FlxSprite
 		setPosition(x, y);
 		alpha = 0.6;
 		animation.play('note' + notedata + '-' + FlxG.random.int(0, 1), true);
-		animation.curAnim.frameRate += FlxG.random.int(-2, 2);
 		updateHitbox();
 		offset.set(width * 0.3, height * 0.3);
 	}
