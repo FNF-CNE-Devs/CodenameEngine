53a98a636e71cd8fe29a10d76828d2c83ed16c80
soy-asterisk <46652463+soy-asterisk@users.noreply.github.com>
2023-01-01 16:39:53 +0900

    Update PlayState.hx

--- source/funkin/game/PlayState.hx
+++ source/funkin/game/PlayState.hx
@@ -604,12 +604,12 @@ class PlayState extends MusicBeatState
 		add(healthBarBG);
 
 		healthBar = new FlxBar(healthBarBG.x + 4, healthBarBG.y + 4, RIGHT_TO_LEFT, Std.int(healthBarBG.width - 8), Std.int(healthBarBG.height - 8), this,
-			'health', 0, SONG.maxHealth);
+			'health', 0, maxHealth);
 		healthBar.scrollFactor.set();
 		healthBar.createFilledBar(0xFFFF0000, 0xFF66FF33);
 		add(healthBar);
 
-		health = SONG.maxHealth / 2;
+		health = maxHealth / 2;
 
 
 		iconP1 = new HealthIcon(boyfriend.getIcon(), true);
