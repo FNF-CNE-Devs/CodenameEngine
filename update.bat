676f9e67414339a94fa5afd407f73baae39058fa
Yoshman29 <81191621+YoshiCrafter29@users.noreply.github.com>
2023-01-06 14:32:22 +0100

    adding hmm support as asked by eric

--- update.bat
+++ update.bat
@@ -1,7 +1,3 @@
-haxelib git linc_discord-rpc https://github.com/Aidan63/linc_discord-rpc
-haxelib git flixel-yc29 https://www.github.com/YoshiCrafter29/flixel
-haxelib git hscript-improved https://www.github.com/YoshiCrafter29/hscript-improved
-haxelib install away3d
-haxelib install hxCodec
-haxelib install markdown
-haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate/
+haxelib install hmm
+haxelib run hmm setup
+hmm install
