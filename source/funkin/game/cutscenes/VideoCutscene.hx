ff2bbcaa9097ee5d6d7ebd1adaed687c170a72d3
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-06-26 07:38:21 +0300

    Update VideoCutscene.hx

--- source/funkin/game/cutscenes/VideoCutscene.hx
+++ source/funkin/game/cutscenes/VideoCutscene.hx
@@ -173,9 +173,7 @@ class VideoCutscene extends Cutscene {
 			loadingBackdrop.x -= elapsed * FlxG.width * 0.5;
 		}
 		if (skippable && video.isPlaying && controls.ACCEPT) {
-			video.stop();
-			video.dispose();
-			close();
+			video.onEndReached.dispatch();
 		}
 		#else
 		close();
