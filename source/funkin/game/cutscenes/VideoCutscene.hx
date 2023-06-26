13c045da5611692e28319608128d24ab137f9005
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-06-26 15:26:49 +0300

    Reorder

--- source/funkin/game/cutscenes/VideoCutscene.hx
+++ source/funkin/game/cutscenes/VideoCutscene.hx
@@ -53,8 +53,8 @@ class VideoCutscene extends Cutscene {
 		parseSubtitles();
 
 		video = new FlxVideo();
-		video.onEndReached.add(close);
 		video.onEndReached.add(video.dispose);
+		video.onEndReached.add(close);
 
 		bg = new FlxSprite(0, FlxG.height * 0.85).makeGraphic(1, 1, 0xFF000000);
 		bg.alpha = 0.5;
