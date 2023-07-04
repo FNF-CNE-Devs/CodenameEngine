8a3c763c6bc1f9ed7cb2782aadaf9ab3f87be84f
Sword352 <99792114+Sword352@users.noreply.github.com>
2023-07-04 08:27:36 +0200

    Revert "Update FramerateCounter.hx"

--- source/funkin/backend/system/framerate/FramerateCounter.hx
+++ source/funkin/backend/system/framerate/FramerateCounter.hx
@@ -30,7 +30,7 @@ class FramerateCounter extends Sprite {
 		if (alpha <= 0.05) return;
 		super.__enterFrame(t);
 
-		lastFPS = FlxG.elapsed == 0 ? 0 : (1 / FlxG.elapsed);
+		lastFPS = CoolUtil.fpsLerp(lastFPS, FlxG.elapsed == 0 ? 0 : (1 / FlxG.elapsed), 0.25);
 		fpsNum.text = Std.string(Math.floor(lastFPS));
 		fpsLabel.x = fpsNum.x + fpsNum.width;
 		fpsLabel.y = (fpsNum.y + fpsNum.height) - fpsLabel.height;
