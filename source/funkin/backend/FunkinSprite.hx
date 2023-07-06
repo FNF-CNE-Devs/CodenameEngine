287457c43a67571b4fc27b132c9183e4ac786440
MemeHovy <124418090+MemeHovy@users.noreply.github.com>
2023-07-05 21:56:13 -0400

    null check

--- source/funkin/backend/FunkinSprite.hx
+++ source/funkin/backend/FunkinSprite.hx
@@ -210,12 +210,14 @@ class FunkinSprite extends FlxSkewedSprite implements IBeatReceiver implements I
 	{
 		animateAtlas = FlxDestroyUtil.destroy(animateAtlas);
 
-		for (key in animOffsets.keys()) {
-			final point = animOffsets[key];
-			animOffsets.remove(key);
-			point.put();
+		if (animOffsets != null){
+			for (key in animOffsets.keys()) {
+				final point = animOffsets[key];
+				animOffsets.remove(key);
+				point.put();
+			}
+			animOffsets = null;
 		}
-		animOffsets = null;
 		super.destroy();
 	}
 	#end
