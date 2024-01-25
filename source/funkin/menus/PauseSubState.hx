a93eb1c040508ef303f5d4d324150d49ddb7b143
Frakist <90983918+Frakist@users.noreply.github.com>
2024-01-25 20:58:43 +0200

    revert

--- source/funkin/menus/PauseSubState.hx
+++ source/funkin/menus/PauseSubState.hx
@@ -27,6 +27,8 @@ class PauseSubState extends MusicBeatSubstate
 
 	var pauseMusic:FlxSound;
 
+	public var pauseScript:Script;
+
 	public var game:PlayState = PlayState.instance; // shortcut
 
 	private var __cancelDefault:Bool = false;
@@ -45,10 +47,12 @@ class PauseSubState extends MusicBeatSubstate
 
 		add(parentDisabler = new FunkinParentDisabler());
 
-		stateScripts.importScript(script);
+		pauseScript = Script.create(Paths.script(script));
+		pauseScript.setParent(this);
+		pauseScript.load();
 
 		var event = EventManager.get(PauseCreationEvent).recycle('breakfast', menuItems);
-		stateScripts.call('onCreatePause', [event]);
+		pauseScript.call('create', [event]);
 
 		menuItems = event.options;
 
@@ -100,6 +104,8 @@ class PauseSubState extends MusicBeatSubstate
 		camera.bgColor = 0;
 		FlxG.cameras.add(camera, false);
 
+		pauseScript.call("postCreate");
+
 		PlayState.instance.updateDiscordPresence();
 	}
 
@@ -109,6 +115,8 @@ class PauseSubState extends MusicBeatSubstate
 
 		if (pauseMusic.volume < 0.5)
 			pauseMusic.volume += 0.01 * elapsed;
+
+		pauseScript.call("update", [elapsed]);
 
 		if (__cancelDefault) return;
 
@@ -126,7 +134,7 @@ class PauseSubState extends MusicBeatSubstate
 
 	public function selectOption() {
 		var event = EventManager.get(NameEvent).recycle(menuItems[curSelected]);
-		stateScripts.call("onSelectOption", [event]);
+		pauseScript.call("onSelectOption", [event]);
 
 		if (event.cancelled) return;
 
@@ -156,6 +164,8 @@ class PauseSubState extends MusicBeatSubstate
 	{
 		if(FlxG.cameras.list.contains(camera))
 			FlxG.cameras.remove(camera, true);
+		pauseScript.call("onDestroy");
+		pauseScript.destroy();
 
 		if (pauseMusic != null)
 			@:privateAccess {
@@ -168,7 +178,7 @@ class PauseSubState extends MusicBeatSubstate
 	function changeSelection(change:Int = 0):Void
 	{
 		var event = EventManager.get(MenuChangeEvent).recycle(curSelected, FlxMath.wrap(curSelected + change, 0, menuItems.length-1), change, change != 0);
-		stateScripts.call("onChangeItem", [event]);
+		pauseScript.call("onChangeItem", [event]);
 		if (event.cancelled) return;
 
 		curSelected = event.value;
