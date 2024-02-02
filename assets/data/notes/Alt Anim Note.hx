0a8d2ff049e06190c76af238d596f5d0e75d3ca8
lunarcleint <88809950+lunarcleint@users.noreply.github.com>
2024-02-01 19:31:34 -0700

    alt anim notesfsa

--- assets/data/notes/Alt Anim Note.hx
+++ assets/data/notes/Alt Anim Note.hx
@@ -1,7 +1,2 @@
-import haxe.io.Path;
-
-function onNoteHit(event) {
-	if(event.note.noteType == Path.withoutExtension(__script__.fileName)) {
-		event.animSuffix = "-alt";
-	}
-}
+//
+function onNoteHit(event) if (event.noteType == "Alt Anim Note") event.animSuffix = "-alt";
