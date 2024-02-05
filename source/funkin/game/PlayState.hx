e9e7daafe047efd710b2a02f20f53575c57e9ef7
lunarcleint <88809950+lunarcleint@users.noreply.github.com>
2024-02-04 21:49:22 -0700

    i can NOT spell

--- source/funkin/game/PlayState.hx
+++ source/funkin/game/PlayState.hx
@@ -1297,7 +1297,7 @@ class PlayState extends MusicBeatState
 				if (strLine != null) {
 					strLine.altAnim = cast event.params[0];
 
-					if (strumlines.characters != null) // Alt anim Idle
+					if (strLine.characters != null) // Alt anim Idle
 						for (character in strLine.characters) {
 							if (character == null) continue;
 							character.idleSuffix = event.params[1] ? "-alt" : "";
