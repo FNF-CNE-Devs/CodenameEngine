ed007215ebb5d1db501a6a9fe500bad4cec1e9dc
KarimAkra <karimakra512@gmail.com>
2024-02-07 17:34:17 +0200

    replace old discord-rpc with hxdiscord_rpc

--- source/funkin/backend/scripting/events/DiscordPresenceUpdateEvent.hx
+++ source/funkin/backend/scripting/events/DiscordPresenceUpdateEvent.hx
@@ -1,12 +1,12 @@
 package funkin.backend.scripting.events;
 
 #if DISCORD_RPC
-import discord_rpc.DiscordRpc.DiscordPresenceOptions;
+//import funkin.backend.utils.DiscordUtil.DiscordPresenceOptions;
 #end
 
 final class DiscordPresenceUpdateEvent extends CancellableEvent {
 	/**
 	 * Object containing all of the data for the presence. Can be altered.
 	 */
-	public var presenceData:#if DISCORD_RPC DiscordPresenceOptions #else Dynamic #end;
+//	public var presenceData:#if DISCORD_RPC DiscordPresenceOptions #else Dynamic #end;
 }
--- source/funkin/backend/utils/DiscordUtil.hx
+++ source/funkin/backend/utils/DiscordUtil.hx
@@ -4,10 +4,11 @@ import funkin.backend.scripting.events.DiscordPresenceUpdateEvent;
 import haxe.Json;
 import flixel.sound.FlxSound;
 #if DISCORD_RPC
-import discord_rpc.DiscordRpc;
 import sys.thread.Thread;
 import Sys;
 import lime.app.Application;
+import hxdiscord_rpc.Discord;
+import hxdiscord_rpc.Types;
 #end
 
 class DiscordUtil {
@@ -15,10 +16,15 @@ class DiscordUtil {
 	public static var discordThread:#if DISCORD_RPC Thread #else Dynamic #end = null;
 	public static var ready:Bool = false;
 	public static var data:DiscordJson = null;
+	private static var presence:DiscordRichPresence = DiscordRichPresence.create();
 
 	public static function init() {
 		#if DISCORD_RPC
 		reloadJsonData();
+		Application.current.onExit.add(function(exitCode) {
+			shutdown();
+		});
+
 		discordThread = Thread.create(function() {
 			while (true)
 			{
@@ -26,13 +32,12 @@ class DiscordUtil {
 					Sys.sleep(1/60);
 				}
 				trace("Processing Discord RPC...");
-				DiscordRpc.process();
+				#if DISCORD_DISABLE_IO_THREAD
+				Discord.UpdateConnection();
+				#end
+				Discord.RunCallbacks();
 				Sys.sleep(2);
 			}
-		});
-
-		Application.current.onExit.add(function(exitCode) {
-			shutdown();
 		});
 		#end
 	}
@@ -58,11 +63,11 @@ class DiscordUtil {
 
 	public static function changePresence(details:String, state:String, ?smallImageKey : String) {
 		#if DISCORD_RPC
-		changePresenceAdvanced({
-			state: state,
-			details: details,
-			smallImageKey: smallImageKey
-		});
+		presence.state = state;
+		presence.details = details;
+		presence.smallImageKey = smallImageKey;
+
+		updatePresence();
 		#end
 	}
 
@@ -76,70 +81,76 @@ class DiscordUtil {
 			end = start + (audio.length - audio.time);
 		}
 
-		changePresenceAdvanced({
-			state: state,
-			details: details,
-			smallImageKey: smallImageKey,
-			startTimestamp: Std.int(start / 1000),
-			endTimestamp: Std.int(end / 1000)
-		});
+		presence.details = details;
+		presence.state = state;
+		//presence.audio = audio; // hxdiscord_rpc dosen't have this ig?
+		presence.smallImageKey = smallImageKey;
+		presence.startTimestamp = Std.int(start / 1000);
+		presence.endTimestamp = Std.int(end / 1000);
+		
+		updatePresence();
 		#end
 	}
 
-	public static function changePresenceAdvanced(data:#if DISCORD_RPC DiscordPresenceOptions #else Dynamic #end) {
+	public static function updatePresence() {
 		#if DISCORD_RPC
-		if (data == null) return;
-
-		if (data.largeImageKey == null)
-			data.largeImageKey = DiscordUtil.data.logoKey;
-		if (data.largeImageText == null)
-			data.largeImageText = DiscordUtil.data.logoText;
+		if (presence.largeImageKey == null){
+			var FUCKOFF:String = data.logoKey;
+			presence.largeImageKey = FUCKOFF;
+		}
+		if (presence.largeImageText == null){
+			var FUCKOFF:String = data.logoText;
+			presence.largeImageText = FUCKOFF;
+		}
 
 		#if GLOBAL_SCRIPT
-		var event = funkin.backend.scripting.GlobalScript.event("onDiscordPresenceUpdate", EventManager.get(DiscordPresenceUpdateEvent).recycle(data));
-		if (event.cancelled) return;
+		//var event = funkin.backend.scripting.GlobalScript.event("onDiscordPresenceUpdate", EventManager.get(DiscordPresenceUpdateEvent).recycle(data));
+		//if (event.cancelled) return;
 		#end
 
-		DiscordRpc.presence(data);
+		Discord.UpdatePresence(cpp.RawConstPointer.addressOf(presence));
 		#end
 	}
 	public static function changeClientID(id:String) {
 		#if DISCORD_RPC
-		if (currentID != null) {
-			DiscordRpc.shutdown();
-		}
-
+		shutdown();
 		ready = false;
 
-		DiscordRpc.start({
-			clientID: id,
-			onReady: function() {
-				Logs.trace('Discord RPC started');
-				ready = true;
-			},
-			onError: onError,
-			onDisconnected: onDisconnected
-		});
+		var discordHandlers:DiscordEventHandlers = DiscordEventHandlers.create();
+		discordHandlers.ready = cpp.Function.fromStaticFunction(onReady);
+		discordHandlers.disconnected = cpp.Function.fromStaticFunction(onDisconnected);
+		discordHandlers.errored = cpp.Function.fromStaticFunction(onError);
+		Discord.Initialize(id, cpp.RawPointer.addressOf(discordHandlers), 1, null);
 		currentID = id;
-
 		#end
 	}
 
 	public static function shutdown() {
 		#if DISCORD_RPC
-		DiscordRpc.shutdown();
+		if (currentID != null)
+			Discord.Shutdown();
 		#end
 	}
 
 
 	// HANDLERS
 	#if DISCORD_RPC
-	static function onError(_code:Int, _message:String) {
-		Logs.trace('Discord RPC Error: ${_message} (Code: $_code)', ERROR);
+	static function onReady(request:cpp.RawConstPointer<DiscordUser>) {
+		var requestPtr:cpp.Star<DiscordUser> = cpp.ConstPointer.fromRaw(request).ptr;
+		Logs.trace('Discord RPC started');
+		if (Std.parseInt(cast(requestPtr.discriminator, String)) != 0) //New Discord IDs/Discriminator system
+			trace('(Discord) Connected to User (${cast(requestPtr.username, String)}#${cast(requestPtr.discriminator, String)})');
+		else //Old discriminators
+			trace('(Discord) Connected to User (${cast(requestPtr.username, String)})');
+		ready = true;
 	}
 
-	static function onDisconnected(_code:Int, _message:String) {
-		Logs.trace('Discord RPC Disconnected: ${_message} (Code: $_code)', WARNING);
+	static function onError(_code:Int, _message:cpp.ConstCharStar) {
+		Logs.trace('Discord RPC Error: ${cast(_message, String)} (Code: $_code)', ERROR);
+	}
+
+	static function onDisconnected(_code:Int, _message:cpp.ConstCharStar) {
+		Logs.trace('Discord RPC Disconnected: ${cast(_message, String)} (Code: $_code)', WARNING);
 	}
 	#end
 }
@@ -148,4 +159,33 @@ typedef DiscordJson = {
 	var ?clientID:String;
 	var ?logoKey:String;
 	var ?logoText:String;
+}
+
+// taken from old discord-rpc...
+typedef DiscordPresenceOptions = {
+	// has to make them all cpp.ConstCharStar cuz 
+	/*
+	Error: DiscordUtil.cpp
+	./src/funkin/backend/utils/DiscordUtil.cpp(259): error C2440: 'initializing': cannot convert from 'hx::Val' to 'const char *'
+	./src/funkin/backend/utils/DiscordUtil.cpp(259): note: No user-defined-conversion operator available that can perform this conversion, or the operator cannot be called
+	./src/funkin/backend/utils/DiscordUtil.cpp(261): error C2440: 'initializing': cannot convert from 'hx::Val' to 'const char *'
+	./src/funkin/backend/utils/DiscordUtil.cpp(261): note: No user-defined-conversion operator available that can perform this conversion, or the operator cannot be called
+	./src/funkin/backend/utils/DiscordUtil.cpp(263): error C2440: 'initializing': cannot convert from 'hx::Val' to 'const char *'
+	./src/funkin/backend/utils/DiscordUtil.cpp(263): note: No user-defined-conversion operator available that can perform this conversion, or the operator cannot be called
+	./src/funkin/backend/utils/DiscordUtil.cpp(265): error C2440: 'initializing': cannot convert from 'hx::Val' to 'const char *'
+	./src/funkin/backend/utils/DiscordUtil.cpp(265): note: No user-defined-conversion operator available that can perform this conversion, or the operator cannot be called
+	./src/funkin/backend/utils/DiscordUtil.cpp(267): error C2440: 'initializing': cannot convert from 'hx::Val' to 'const char *'
+	./src/funkin/backend/utils/DiscordUtil.cpp(267): note: No user-defined-conversion operator available that can perform this conversion, or the operator cannot be called
+	./src/funkin/backend/utils/DiscordUtil.cpp(269): error C2440: 'initializing': cannot convert from 'hx::Val' to 'const char *'
+	./src/funkin/backend/utils/DiscordUtil.cpp(269): note: No user-defined-conversion operator available that can perform this conversion, or the operator cannot be called
+	*/
+
+    @:optional var state:cpp.ConstCharStar; // String
+    @:optional var details:cpp.ConstCharStar; // String
+    @:optional var startTimestamp:Dynamic; // Int uhh haxe said to use cpp.Int64 here dunno
+    @:optional var endTimestamp:Dynamic; // Int
+    @:optional var largeImageKey:cpp.ConstCharStar; // String
+    @:optional var largeImageText:cpp.ConstCharStar; // String
+    @:optional var smallImageKey:cpp.ConstCharStar; // String
+    @:optional var smallImageText:cpp.ConstCharStar; // String
 }
