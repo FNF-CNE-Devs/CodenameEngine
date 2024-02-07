9ab1812c3ce425ce2badd48a5607d606e5881c1e
Lily <egzozu.be.bas@gmail.com>
2024-02-07 14:10:25 +0300

    Update BetaWarningState.hx

--- source/funkin/menus/BetaWarningState.hx
+++ source/funkin/menus/BetaWarningState.hx
@@ -20,7 +20,7 @@ class BetaWarningState extends MusicBeatState {
 
 		disclaimer = new FunkinText(16, titleAlphabet.y + titleAlphabet.height + 10, FlxG.width - 32, "", 32);
 		disclaimer.alignment = CENTER;
-		disclaimer.applyMarkup("This engine is still in a beta state. That means *majority of the features* are either *buggy* or *non finished*. If you find any bugs, please report them to the Codename Engine GitHub.\n\nPress ENTER to continue",
+		disclaimer.applyMarkup("This engine is still in a beta state. That means *majority of the features* are either *buggy* or *non finished*. If you find any bugs, please report them to the Codename Engine GitHub.\n\nPress ENTER or Touch Your Screen to continue",
 			[
 				new FlxTextFormatMarkerPair(new FlxTextFormat(0xFFFF4444), "*")
 			]
@@ -34,6 +34,22 @@ class BetaWarningState extends MusicBeatState {
 
 	public override function update(elapsed:Float) {
 		super.update(elapsed);
+
+		#if FLX_TOUCH
+		for (touch in FlxG.touches.list)
+		{
+			if (touch.justPressed && transitioning) {
+				FlxG.camera.stopFX(); FlxG.camera.visible = false;
+				goToTitle();
+			} elseif (touch.justPressed && !transitioning) {
+				transitioning = true;
+				CoolUtil.playMenuSFX(CONFIRM);
+				FlxG.camera.flash(FlxColor.WHITE, 1, function() {
+					FlxG.camera.fade(FlxColor.BLACK, 2.5, false, goToTitle);
+				});
+			}
+		}
+		#end
 
 		if (controls.ACCEPT && transitioning) {
 			FlxG.camera.stopFX(); FlxG.camera.visible = false;
