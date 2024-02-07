31feab6cfd235d3d7fbe3bace9a4eb9dc55e69ca
crowplexus <45212377+crowplexus@users.noreply.github.com>
2024-02-07 19:42:08 -0400

    Update CodenameBuildField.hx

--- source/funkin/backend/system/framerate/CodenameBuildField.hx
+++ source/funkin/backend/system/framerate/CodenameBuildField.hx
@@ -6,11 +6,15 @@ import openfl.text.TextField;
 import funkin.backend.system.macros.GitCommitMacro;
 
 class CodenameBuildField extends TextField {
+	// make this empty once you guys are done with the project.
+	// good luck /gen <3 @crowplexus
+	public static final releaseCycle:String = " Beta ";
+
 	public function new() {
 		super();
 		defaultTextFormat = Framerate.textFormat;
 		autoSize = LEFT;
 		multiline = wordWrap = false;
-		text = 'Codename Engine Beta\nCommit ${GitCommitMacro.commitNumber} (${GitCommitMacro.commitHash})';
+		text = 'Codename Engine${releaseCycle}\nCommit ${GitCommitMacro.commitNumber} (${GitCommitMacro.commitHash})';
 	}
 }
