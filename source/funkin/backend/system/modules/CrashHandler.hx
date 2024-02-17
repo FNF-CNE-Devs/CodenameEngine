5ee72c136901965d9571027f4a46176ef5b8e6e0
Lily <egzozu.be.bas@gmail.com>
2024-02-17 17:19:07 +0300

    Update CrashHandler.hx

--- source/funkin/backend/system/modules/CrashHandler.hx
+++ source/funkin/backend/system/modules/CrashHandler.hx
@@ -1,72 +1,133 @@
 package funkin.backend.system.modules;
 
-import lime.system.System;
-import funkin.backend.utils.NativeAPI;
-import openfl.Lib;
-import openfl.events.UncaughtErrorEvent;
-import openfl.events.ErrorEvent;
-import openfl.errors.Error;
-import openfl.events.UncaughtErrorEvent;
 import haxe.CallStack;
+import haxe.Exception;
+import openfl.errors.Error;
+import openfl.events.ErrorEvent;
+import lime.utils.Log as LimeLogger;
+import openfl.events.UncaughtErrorEvent;
+import lime.system.System as LimeSystem;
 
-class CrashHandler {
-	public static function init() {
-		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onUncaughtError);
-		#if cpp
-		untyped __global__.__hxcpp_set_critical_error_handler(onError);
-		#elseif hl
-		hl.Api.setErrorHandler(onError);
-		#end
-	}
-
-	public static function onUncaughtError(e:UncaughtErrorEvent) {
-		var m:String = e.error;
-		if (Std.isOfType(e.error, Error)) {
-			var err = cast(e.error, Error);
-			m = '${err.message}';
-		} else if (Std.isOfType(e.error, ErrorEvent)) {
-			var err = cast(e.error, ErrorEvent);
-			m = '${err.text}';
-		}
-		var stack = CallStack.exceptionStack();
-		var stackLabel:String = "";
-		for(e in stack) {
-			switch(e) {
-				case CFunction: stackLabel += "Non-Haxe (C) Function";
-				case Module(c): stackLabel += 'Module ${c}';
-				case FilePos(parent, file, line, col):
-					switch(parent) {
-						case Method(cla, func):
-							stackLabel += '(${file}) ${cla.split(".").last()}.$func() - line $line';
-						case _:
-							stackLabel += '(${file}) - line $line';
-					}
-				case LocalFunction(v):
-					stackLabel += 'Local Function ${v}';
-				case Method(cl, m):
-					stackLabel += '${cl} - ${m}';
-			}
-			stackLabel += "\r\n";
-		}
-
-		/*e.preventDefault();
-		e.stopPropagation();
-		e.stopImmediatePropagation();*/
-
-		NativeAPI.showMessageBox("Codename Engine Crash Handler", 'Uncaught Error:$m\n\n$stackLabel', MSG_ERROR);
-		#if sys
-		Sys.exit(1);
-		#end
-	}
-
-	#if (cpp || hl)
-	private static function onError(message:Dynamic):Void
+/**
+ * A uncaught error handler.
+ * @author: Sqirra-RNG, YoshiCrafter29, Mihai Alexandru (M.A. Jigsaw) and Lily Ross (mcagabe19)
+ */
+class CrashHandler
+{
+	/**
+	 * Init of the Crash Handler.
+	 */
+	public static function init():Void
 	{
-		#if mobile
-		NativeAPI.showMessageBox("Codename Engine Crash Handler", '$message', MSG_ERROR);
-		#else
-		throw Std.string(message);
+		openfl.Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onError);
+		#if cpp
+		untyped __global__.__hxcpp_set_critical_error_handler(onCriticalError);
+		#elseif hl
+		hl.Api.setErrorHandler(onCriticalError);
 		#end
 	}
-	#end
+
+	private static function onError(event:UncaughtErrorEvent):Void
+	{
+		event.preventDefault();
+		event.stopImmediatePropagation();
+
+		final log:Array<String> = [];
+
+		if (Std.isOfType(event.error, Error))
+			log.push(cast(event.error, Error).message);
+		else if (Std.isOfType(event.error, ErrorEvent))
+			log.push(cast(event.error, ErrorEvent).text);
+		else
+			log.push(Std.string(event.error));
+
+		for (item in CallStack.exceptionStack(true))
+		{
+			switch (item)
+			{
+				case CFunction:
+					log.push('C Function');
+				case Module(m):
+					log.push('Module [$m]');
+				case FilePos(s, file, line, column):
+					log.push('$file [line $line column $column]');
+				case Method(classname, method):
+					log.push('$classname [method $method]');
+				case LocalFunction(name):
+					log.push('Local Function [$name]');
+			}
+		}
+
+		final msg:String = log.join('\n');
+
+		#if sys
+		try
+		{
+			if (!FileSystem.exists('crash'))
+				FileSystem.createDirectory('crash');
+
+			File.saveContent('crash/' + Date.now().toString().replace(' ', '-').replace(':', "'") + '.txt', msg);
+		}
+		catch (e:Exception)
+			trace('Couldn\'t save error message. (${e.message})', null);
+		#end
+
+		NativeAPI.showMessageBox("Error!", msg, MSG_ERROR);
+
+		#if js
+		if (FlxG.sound.music != null)
+			FlxG.sound.music.stop();
+
+		js.Browser.window.location.reload(true);
+		#else
+		LimeSystem.exit(1);
+		#end
+	}
+
+	private static inline function onCriticalError(error:Dynamic):Void
+	{
+		final log:Array<String> = [Std.isOfType(error, String) ? error : Std.string(error)];
+
+		for (item in CallStack.exceptionStack(true))
+		{
+			switch (item)
+			{
+				case CFunction:
+					log.push('C Function');
+				case Module(m):
+					log.push('Module [$m]');
+				case FilePos(s, file, line, column):
+					log.push('$file [line $line column $column]');
+				case Method(classname, method):
+					log.push('$classname [method $method]');
+				case LocalFunction(name):
+					log.push('Local Function [$name]');
+			}
+		}
+
+		final msg:String = log.join('\n');
+
+		#if sys
+		try
+		{
+			if (!FileSystem.exists('crash'))
+				FileSystem.createDirectory('crash');
+
+			File.saveContent('crash/' + Date.now().toString().replace(' ', '-').replace(':', "'") + '-critical' + '.txt', msg);
+		}
+		catch (e:Exception)
+			trace('Couldn\'t save error message. (${e.message})', null);
+		#end
+
+		NativeAPI.showMessageBox("Critical Error!", msg, MSG_ERROR);
+
+		#if js
+		if (FlxG.sound.music != null)
+			FlxG.sound.music.stop();
+
+		js.Browser.window.location.reload(true);
+		#else
+		LimeSystem.exit(1);
+		#end
+	}
 }
