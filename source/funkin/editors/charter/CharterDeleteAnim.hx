8681fc503e10bec678bd2253f3ae0de15aa02350
lunarcleint <88809950+lunarcleint@users.noreply.github.com>
2024-03-21 14:17:51 -0700

    tweak delete anim a bit

--- source/funkin/editors/charter/CharterDeleteAnim.hx
+++ source/funkin/editors/charter/CharterDeleteAnim.hx
@@ -2,7 +2,7 @@ package funkin.editors.charter;
 
 class CharterDeleteAnim extends CharterNote {
 	public var deleteNotes:Array<{note:CharterNote, time:Float}> = [];
-	public var deleteTime:Float = .5;
+	public var deleteTime:Float = .4;
 
 	public function new() {
 		super();
@@ -21,21 +21,21 @@ class CharterDeleteAnim extends CharterNote {
 
 	public override function draw() @:privateAccess {
 		for (deleteData in deleteNotes) {
-			y = deleteData.note.y + (deleteData.time>deleteTime*.6 ? (deleteData.time/deleteTime)*FlxG.random.float(-1.5, 1.5) : 0); // lunar when no shake :(( 
-			x = deleteData.note.x + (deleteData.time>deleteTime*.6 ? (deleteData.time/deleteTime)*FlxG.random.float(-1.5, 1.5) : 0); // lunar when no shake :((
+			y = deleteData.note.y + (deleteData.time>deleteTime*.5 ? (deleteData.time/deleteTime)*FlxG.random.float(-1.1, 1.1) : 0); // lunar when no shake :(( 
+			x = deleteData.note.x + (deleteData.time>deleteTime*.5 ? (deleteData.time/deleteTime)*FlxG.random.float(-1.1, 1.1) : 0); // lunar when no shake :((
 			angle = deleteData.note.angle; alpha = 1;
 			animation.curAnim.curFrame = 3;
 
 			sustainSpr.scale.set(10, (40 * deleteData.note.susLength) + (height/2));
 			sustainSpr.updateHitbox(); sustainSpr.follow(this, 15, 20);
-			sustainSpr.exists = deleteData.note.susLength != 0; sustainSpr.alpha = .4;
+			sustainSpr.exists = deleteData.note.susLength != 0; sustainSpr.alpha = .8;
 
 			typeText.text = Std.string(deleteData.note.type);
 			typeText.exists = deleteData.note.type != 0; typeText.alpha = .4;
 			typeText.follow(this, 20 - (typeText.frameWidth/2), 20 - (typeText.frameHeight/2));
 
 			for (member in [this, sustainSpr, typeText]) 
-				member.alpha *= deleteData.time/deleteTime;
+				member.alpha *= FlxEase.quadInOut(deleteData.time/deleteTime);
 
 			super.draw();
 		}
