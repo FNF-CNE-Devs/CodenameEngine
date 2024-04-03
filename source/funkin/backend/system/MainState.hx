9e7dfaceefac5d97ae7bd6ef7bf5bef26280fd37
Lily <egzozu.be.bas@gmail.com>
2024-04-03 07:42:52 +0300

    Update MainState.hx

--- source/funkin/backend/system/MainState.hx
+++ source/funkin/backend/system/MainState.hx
@@ -16,7 +16,6 @@ class MainState extends FlxState {
 	public static var initiated:Bool = false;
 	public static var betaWarningShown:Bool = false;
 	public override function create() {
-		flixel.text.Uwuifier.disableUWU = true;
 		super.create();
 		if (!initiated)
 			Main.loadGameSettings();
