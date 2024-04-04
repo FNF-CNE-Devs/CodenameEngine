913b81440336ac11cd9a54c5bb0b7f8b55aab10d
Lily <egzozu.be.bas@gmail.com>
2024-04-04 15:11:28 +0300

    Create CNEJni.hx

---
+++ CNEJNI.hx
@@ -0,0 +1,15 @@
+// fully stolen from PsychJNI bleh
+
+package mobile.utils;
+
+#if android
+import lime.system.JNI;
+
+class CNEJNI #if (lime >= "8.0.0") implements JNISafety #end
+{
+	@:noCompletion private static var setOrientation_jni:Dynamic = JNI.createStaticMethod('org/libsdl/app/SDLActivity', 'setOrientation', '(IIZLjava/lang/String;)V');
+
+	public static inline function setOrientation(width:Int, height:Int, resizeable:Bool, hint:String):Dynamic
+		return setOrientation_jni(width, height, resizeable, hint);
+}
+#end
