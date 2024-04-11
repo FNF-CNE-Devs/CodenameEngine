3687f6e3be0e9d2344f01d0a032744705eff6ab0
Lily <egzozu.be.bas@gmail.com>
2024-04-11 23:38:51 +0300

    deadly mistake

--- source/funkin/backend/assets/ScriptedAssetLibrary.hx
+++ source/funkin/backend/assets/ScriptedAssetLibrary.hx
@@ -22,7 +22,7 @@ class ScriptedAssetLibrary extends ModsFolderLibrary {
 	private static var nullValue:Dynamic = {};
 
 	public function new(scriptName:String, args:Array<Dynamic> = null, folderPath:String="", libName:String="assets", ?modName:String) {
-		if(folderPath == null) folderPath = #if !mobile "./" + #end "assets/";
+		if(folderPath == "") folderPath = #if mobile SUtil.getStorageDirectory() + #else "./" + #end "assets/";
 		if(modName == null) modName = scriptName;
 		super(folderPath, libName, modName);
 		this.scriptName = scriptName;
