f9b7d4cf45d5eed5f1fdcede57a781199ee8c513
Lily <egzozu.be.bas@gmail.com>
2024-04-11 17:09:19 +0300

    credits buttons

--- source/funkin/menus/credits/CreditsMain.hx
+++ source/funkin/menus/credits/CreditsMain.hx
@@ -41,9 +41,8 @@ class CreditsMain extends TreeMenu {
 			CoolUtil.openURL("https://ninja-muffin24.itch.io/funkin");
 		}));
 
-		main = new OptionsScreen('Credits', 'The people who made this possible!', selectables);
+		main = new OptionsScreen('Credits', 'The people who made this possible!', selectables, 'UP_DOWN', 'A_B');
 
-		addVirtualPad('UP_DOWN', 'A_B');
 		super.create();
 
 		DiscordUtil.call("onMenuLoaded", ["Credits Menu"]);
@@ -92,7 +91,7 @@ class CreditsMain extends TreeMenu {
 
 					case "menu":
 						credsMenus.push(new TextOption(name + " >", desc, function() {
-							optionsTree.add(new OptionsScreen(name, desc, parseCreditsFromXML(node, source)));
+							optionsTree.add(new OptionsScreen(name, desc, parseCreditsFromXML(node, source), 'UP_DOWN', 'A_B'));
 						}));
 				}
 			}
