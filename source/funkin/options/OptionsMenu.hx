bf0a6a9bbe48bc5eb6df56077b66ecac7cbbcedd
syrup <123270096+maplesyruppppppp@users.noreply.github.com>
2024-06-07 06:32:05 -0500

    FPS Counter offset fix for Options/Credits menu (#307)
    * pixel note splashes
    
    * tyy!!
    
    * pixel secret notesplashes
    
    * the fix itself lol

--- source/funkin/options/OptionsMenu.hx
+++ source/funkin/options/OptionsMenu.hx
@@ -1,6 +1,5 @@
 package funkin.options;
 
-import funkin.backend.system.framerate.Framerate;
 import funkin.options.type.Checkbox;
 import haxe.xml.Access;
 import funkin.options.type.*;
@@ -47,8 +46,6 @@ class OptionsMenu extends TreeMenu {
 		bg.scrollFactor.set();
 		bg.antialiasing = true;
 		add(bg);
-
-		Framerate.offset.y = 60;
 
 		main = new OptionsScreen("Options", "Select a category to continue.", [for(o in mainOptions) new TextOption(o.name, o.desc, function() {
 			if (o.substate != null) {
