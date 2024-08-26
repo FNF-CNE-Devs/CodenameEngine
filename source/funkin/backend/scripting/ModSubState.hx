122412056eb60572a0e0ca33ba3486d76b2775be
ニコラス★ <61890175+Nikorasu-d@users.noreply.github.com>
2024-08-26 13:23:10 -0400

    Added Optional JSON Data to ModStates/ModSubState

--- source/funkin/backend/scripting/ModSubState.hx
+++ source/funkin/backend/scripting/ModSubState.hx
@@ -2,9 +2,20 @@ package funkin.backend.scripting;
 
 class ModSubState extends MusicBeatSubstate {
 	public static var lastName:String = null;
-	public function new(stateName:String) {
-		if (stateName != null)
+	public static var data:Dynamic = [];
+
+	//New Constructor
+	public function new(stateName:String, ?jsonData:Dynamic = null) {
+		
+		//State Name
+		if (stateName != null){
 			lastName = stateName;
+		}
+		
+		//Extra Data
+		if(jsonData != null){
+			data = jsonData;
+		}
 		super(true, lastName);
 	}
 }
