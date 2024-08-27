81a74d28fb2a8033911e73f204423c15b92ab8a2
ニコラス★ <61890175+Nikorasu-d@users.noreply.github.com>
2024-08-27 00:07:35 -0400

    Enhanced Comments

--- source/funkin/backend/scripting/ModState.hx
+++ source/funkin/backend/scripting/ModState.hx
@@ -1,9 +1,24 @@
 package funkin.backend.scripting;
 
 class ModState extends MusicBeatState {
-	public static var lastName:String = null;
-	public static var data:Dynamic = null; //Use this to pass data between states (Optional)
 
+	/**
+	* Name of HScript file in assets/data/states.
+	*/
+	public static var lastName:String = null;
+	
+	/**
+	* Optional extra data.
+	*/
+	public static var data:Dynamic = null;
+
+	/**
+	* ModState Constructor.
+	* Inherits from MusicBeatState and allows the execution of an HScript from assets/data/states passed via parameters.
+	* 
+	* @param _stateName Name or path to a HScript file from assets/data/states.
+	* @param _data Optional extra Dynamic data passed from a previous state (JSON suggested).
+	*/
 	public function new(_stateName:String, ?_data:Dynamic) {
 		if(_stateName != null && _stateName != lastName) {
 			lastName = _stateName;
--- source/funkin/backend/scripting/ModSubState.hx
+++ source/funkin/backend/scripting/ModSubState.hx
@@ -1,9 +1,24 @@
 package funkin.backend.scripting;
 
 class ModSubState extends MusicBeatSubstate {
-	public static var lastName:String = null;
-	public static var data:Dynamic = null; //Use this to pass info between states (Optional)
 
+	/**
+	* Name of HScript file in assets/data/states.
+	*/
+	public static var lastName:String = null;
+
+	/**
+	* Optional extra data.
+	*/
+	public static var data:Dynamic = null;
+
+	/**
+	* ModSubState Constructor.
+	* Inherits from MusicBeatSubstate and allows the execution of an HScript from assets/data/states passed via parameters.
+	* 
+	* @param _stateName Name or path to a HScript file from assets/data/states.
+	* @param _data Optional extra Dynamic data passed from a previous state (JSON suggested). 
+	*/
 	public function new(_stateName:String, ?_data:Dynamic) {
 		if(_stateName != null && _stateName != lastName) {
 			lastName = _stateName;
