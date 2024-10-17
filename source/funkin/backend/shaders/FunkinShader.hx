9b80a3feb6768f1b13085cc640361d637480491d
Lily <egzozu.be.bas@gmail.com>
2024-10-17 15:56:46 +0300

    wait that's for gles 3.0

--- source/funkin/backend/shaders/FunkinShader.hx
+++ source/funkin/backend/shaders/FunkinShader.hx
@@ -105,18 +105,8 @@ class FunkinShader extends FlxShader implements IHScriptCustomBehaviour {
 				+ "#endif\n\n";
 			#end
 
-			#if lime_opengles
-			prefix += 'out vec4 output_FragColor;\n';
-			var vertex = prefix
-				+ glVertexSource.replace("attribute", "in")
-					.replace("varying", "out")
-					.replace("texture2D", "texture")
-					.replace("gl_FragColor", "output_FragColor");
-			var fragment = prefix + glFragmentSource.replace("varying", "in").replace("texture2D", "texture").replace("gl_FragColor", "output_FragColor");
-			#else
 			var vertex = prefix + glVertexSource;
 			var fragment = prefix + glFragmentSource;
-			#end
 
 			var id = vertex + fragment;
 
