c151bb2958f07ebba125ea6d744a1852c013bbd4
⍚~Nex <87421482+NexIsDumb@users.noreply.github.com>
2024-11-14 01:11:57 +0100

    typo

--- source/funkin/backend/scripting/events/dialogue/DialogueNextLineEvent.hx
+++ source/funkin/backend/scripting/events/dialogue/DialogueNextLineEvent.hx
@@ -5,7 +5,7 @@ import funkin.game.cutscenes.DialogueCutscene.DialogueLine;
 /**
  * CANCEL this event to prevent continuing with the next dialogue.
  *
- * **NOTE**: To know the current, past or next lines, use `curLine`, `lastLine` or also `dialogueLines` in the `DialogueCutscene` class (they're globals and can be at any time as long as the Dialogue Cutscene is active)!
+ * **NOTE**: To know the current, past or next lines, use `curLine`, `lastLine` or also `dialogueLines` in the `DialogueCutscene` class (they're globals and can be accessed at any time as long as the Dialogue Cutscene is active)!
  */
 final class DialogueNextLineEvent extends CancellableEvent
 {
