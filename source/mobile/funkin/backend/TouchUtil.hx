fb2fe82e00eacbae0961a989f62221c325589ac2
Lily <egzozu.be.bas@gmail.com>
2024-11-14 18:36:07 +0300

    starting to license our classes

---
+++ source/mobile/funkin/backend/TouchUtil.hx
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2024 Mobile Porting Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package mobile.funkin.backend;
+
+import flixel.FlxObject;
+import flixel.input.touch.FlxTouch;
+
+/**
+ * ...
+ * @author: Karim Akra
+ */
+class TouchUtil
+{
+	public static var pressed(get, never):Bool;
+	public static var justPressed(get, never):Bool;
+	public static var justReleased(get, never):Bool;
+	public static var released(get, never):Bool;
+	public static var touch(get, never):FlxTouch;
+
+	public static function overlaps(object:FlxObject, ?camera:FlxCamera):Bool
+	{
+		for (touch in FlxG.touches.list)
+			if (touch.overlaps(object, camera ?? object.camera))
+				return true;
+
+		return false;
+	}
+
+	public static function overlapsComplex(object:FlxObject, ?camera:FlxCamera):Bool
+	{
+		if (camera == null)
+			for (camera in object.cameras)
+				for (touch in FlxG.touches.list)
+					@:privateAccess
+					if (object.overlapsPoint(touch.getWorldPosition(camera, object._point), true, camera))
+						return true;
+		else
+			@:privateAccess
+			if (object.overlapsPoint(touch.getWorldPosition(camera, object._point), true, camera))
+				return true;
+
+		return false;
+	}
+
+	@:noCompletion
+	private static function get_pressed():Bool
+	{
+		for (touch in FlxG.touches.list)
+			if (touch.pressed)
+				return true;
+
+		return false;
+	}
+
+	@:noCompletion
+	private static function get_justPressed():Bool
+	{
+		for (touch in FlxG.touches.list)
+			if (touch.justPressed)
+				return true;
+
+		return false;
+	}
+
+	@:noCompletion
+	private static function get_justReleased():Bool
+	{
+		for (touch in FlxG.touches.list)
+			if (touch.justReleased)
+				return true;
+
+		return false;
+	}
+
+	@:noCompletion
+	private static function get_released():Bool
+	{
+		for (touch in FlxG.touches.list)
+			if (touch.released)
+				return true;
+
+		return false;
+	}
+
+	@:noCompletion
+	private static function get_touch():FlxTouch
+	{
+		for (touch in FlxG.touches.list)
+			if (touch != null)
+				return touch;
+
+		return FlxG.touches.getFirst();
+	}
+}
