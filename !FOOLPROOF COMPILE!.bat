5a0531cd189583ce34dfc3ab308c9b702af83ebe
Danny <67078432+FunnyDannyG@users.noreply.github.com>
2024-12-08 00:16:20 -0500

    wah wah wah

--- !FOOLPROOF COMPILE!.bat
+++ !FOOLPROOF COMPILE!.bat
@@ -1,7 +1,5 @@
 @echo off
 
-goto check_Permissions
-
 :check_Permissions
     net session >nul 2>&1
     if %errorLevel% == 0 (
@@ -14,12 +12,14 @@ goto check_Permissions
 
 haxe --version | findstr "4.2.5" >nul
 if errorlevel 1 (
+	goto check_Permissions
 	echo Downloading Haxe 4.2.5...
 	powershell -Command "& {Invoke-WebRequest -Uri 'https://github.com/HaxeFoundation/haxe/releases/download/4.2.5/haxe-4.2.5-win64.exe' -OutFile 'haxe-installer.exe'; Start-Process 'haxe-installer.exe' -Wait -NoNewWindow;}"
 )
 
 git --version >nul 2>&1
 if errorlevel 1 (
+	goto check_Permissions
     echo Installing Git-SCM...
     powershell -Command "& {Invoke-WebRequest -Uri 'https://git-scm.com/download/win' -OutFile 'Git-Installer.exe'; Start-Process 'Git-Installer.exe' -Wait -NoNewWindow;}"
 )
