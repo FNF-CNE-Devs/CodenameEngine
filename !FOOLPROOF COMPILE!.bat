d41cac0c34cb56b141a0632f45d82aeb732f4ad3
Danny <67078432+FunnyDannyG@users.noreply.github.com>
2024-12-08 00:19:58 -0500

    irts donw

--- !FOOLPROOF COMPILE!.bat
+++ !FOOLPROOF COMPILE!.bat
@@ -1,25 +1,26 @@
 @echo off
 
-:check_Permissions
-    net session >nul 2>&1
+haxe --version | findstr "4.2.5" >nul
+if errorlevel 1 (
+	net session >nul 2>&1
     if %errorLevel% == 0 (
         echo sprunki is good
     ) else (
         echo RUN THIS AS ADMINISTRATOR OR I WILL CASTRATE YOU!!
     )
-    
     pause >nul
-
-haxe --version | findstr "4.2.5" >nul
-if errorlevel 1 (
-	goto check_Permissions
 	echo Downloading Haxe 4.2.5...
 	powershell -Command "& {Invoke-WebRequest -Uri 'https://github.com/HaxeFoundation/haxe/releases/download/4.2.5/haxe-4.2.5-win64.exe' -OutFile 'haxe-installer.exe'; Start-Process 'haxe-installer.exe' -Wait -NoNewWindow;}"
 )
 
 git --version >nul 2>&1
 if errorlevel 1 (
-	goto check_Permissions
+	net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo sprunki is good
+    ) else (
+        echo RUN THIS AS ADMINISTRATOR OR I WILL CASTRATE YOU!!
+    )
     echo Installing Git-SCM...
     powershell -Command "& {Invoke-WebRequest -Uri 'https://git-scm.com/download/win' -OutFile 'Git-Installer.exe'; Start-Process 'Git-Installer.exe' -Wait -NoNewWindow;}"
 )
